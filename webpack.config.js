665ff2915f7dd54cfb3663cfc3a0e491fa776f20
romanlabish <roman.labish@gmail.com>
2016-03-12 01:31:21 +0200

    added empty  folders structure and updated package and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,9 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      {test: /\.css$/, loader: "style-loader!css-loader"}
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
-}
+};
